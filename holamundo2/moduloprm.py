40c1dfffc1406a2f0b461b1b8832b06f29d60c45
tuentconsulting <tuent.consulting@gmail.com>
2016-09-19 01:18:07 +0200

    lo intento de nuevo 1 17

--- holamundo2/moduloprm.py
+++ holamundo2/moduloprm.py
@@ -1,10 +1,10 @@
 '''
 Created on 13 de sept. de 2016
 
-@author: pablo rivera ++
+@author: pablo rivera riiiiiiv
 '''
 from pip._vendor.distlib.compat import raw_input
 print ("hola mundo")
 print ("prebbbbb")
 raw_input()
-print ("taca")
+print ("tacadesdeeclipse")
